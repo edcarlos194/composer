c3f56bd2bb374e04b3d48ea4e71a2a243392ddc5
Ed Carlos Lima De Sousa <23270@etehort.ps.sp>
2024-10-09 22:33:55 -0300

    alteração 04

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,4 +1,17 @@
-
-@foreach ($users as $user)
-    {{ $user->name }}<br/>
-@endforeach
+<style>
+    tr:nth-child(even) {
+        background-color: #00fabb;
+    }
+    tr:nth-child(odd) {
+        background-color: white;
+    }
+</style>
+<h1>Usuários</h1>
+<table>
+    @foreach ($users as $user)
+        <tr>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+        </tr>
+    @endforeach
+</table>
